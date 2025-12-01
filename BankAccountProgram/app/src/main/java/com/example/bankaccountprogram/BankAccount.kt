52b84e5bb5df7c4123c67396068cf87791a7ff27
Nasim-Hasan <nasimcse2k5@gmail.com>
2025-12-02 03:46:58 +0600

    New Kotlin Files with Modified Codes

--- BankAccountProgram/app/src/main/java/com/example/bankaccountprogram/BankAccount.kt
+++ BankAccountProgram/app/src/main/java/com/example/bankaccountprogram/BankAccount.kt
@@ -1,15 +1,31 @@
 package com.example.bankaccountprogram
 
-class BankAccount (val accountHolder:String,val balance:Double) {
+class BankAccount (val accountHolder:String,var balance:Double) {
     private val transactionHistory = mutableListOf<String>();
 
     fun deposit(amount:Double){
+        if(amount>0){
+            balance+=amount;
+            transactionHistory.add("Deposit: $amount");
+        }else{
+            println("Invalid deposit amount");
+        }
 
     }
     fun withdraw (amount:Double){
+        if(amount>0 && amount<=balance) {
+            balance -= amount;
+            transactionHistory.add("Withdraw: $amount");
+        }else{
+            println("Invalid withdraw amount or insufficient balance");
+        }
 
     }
     fun displayTransactionHistory(){
+        println("Transaction History for $accountHolder:");
+        transactionHistory.forEach{
+            println(it);
+        }
 
     }
 }
--- BankAccountProgram/app/src/main/java/com/example/bankaccountprogram/BankMain.kt
+++ BankAccountProgram/app/src/main/java/com/example/bankaccountprogram/BankMain.kt
@@ -4,4 +4,12 @@ fun main(){
     val bankAccount = BankAccount("Nasim Hasan",450.78);
     println("Account Holder's Name:"+bankAccount.accountHolder+
             " , Current Balance="+bankAccount.balance);
+    bankAccount.deposit(0.0);
+    bankAccount.deposit(100.0);
+    bankAccount.withdraw(-100.0);
+    bankAccount.withdraw(1200.0);
+    bankAccount.withdraw(200.0);
+    bankAccount.displayTransactionHistory();
+    println("After the transactions "+bankAccount.accountHolder+
+            "'s Current Balance="+bankAccount.balance);
 }
